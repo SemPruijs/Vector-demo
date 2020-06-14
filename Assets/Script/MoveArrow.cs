2a987857cce9d62da385d29ca00fa3ffc921bdab
SemPruijs <sem@pruijs.nl>
2020-06-14 11:46:11 +0200

    Add project

---
+++ Assets/Script/MoveArrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveArrow : MonoBehaviour
+{
+    // public GameObject directionPoint;
+    public GameObject arrow;
+    
+    public Vector3 arrowPosition;
+    public Vector3 centerPosition;
+    
+    public Slider xPositionSlider;
+    public Slider yPositionSlider;
+
+    public Slider xSlider;
+    public Slider ySlider;
+
+    public Slider scaler;
+
+
+    void Update()
+    {
+        Vector3 scale = new Vector3(arrow.transform.localScale.x, scaler.value, arrow.transform.localScale.y);
+        arrowPosition.x = centerPosition.x + xPositionSlider.value;
+        arrowPosition.y = centerPosition.y + yPositionSlider.value;
+        
+        arrow.transform.position = arrowPosition;
+        arrow.transform.localScale = scale;
+    }
+}
